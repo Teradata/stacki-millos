08416989e96ddc31d3ff373c29debb8013ec3eea
Greg Bruno <root@rhel6-build.stacki.com>
2011-11-06 10:49:15 -0800

    get the OS roll under source control

---
+++ version.mk
@@ -0,0 +1,19 @@
+ROLLNAME	= os
+RELEASE		= 0
+COLOR		= white
+OSNAME		= CentOS
+OSVERSION 	= 6.0
+UPDATESNAME	= Updates-$(OSNAME)-$(OSVERSION)
+
+ROLLDIR		= $(shell rocks report distro)/rolls
+
+CENTOS_VERSION	= \
+	$(shell rocks list roll $(OSNAME) output-header=no output-col=version)
+UPDATES_VERSION	= \
+	$(shell rocks list roll $(UPDATESNAME) output-header=no output-col=version)
+KERNEL_VERSION	= \
+	$(shell rocks list roll rocks+kernel output-header=no output-col=version)
+
+ROLLS		= $(OSNAME),$(CENTOS_VERSION) $(UPDATESNAME),$(UPDATES_VERSION)
+REDHAT.ROOT	= $(PWD)
+
