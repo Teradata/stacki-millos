93bd3887b23b9d43cab88d8996d71d3384c83b1b
Greg Bruno <greg.bruno@stackiq.com>
2014-02-11 11:00:26 -0800

    closer

--- version.mk
+++ version.mk
@@ -1,16 +1,20 @@
 ROLL		= os
 COLOR		= white
 OSNAME		= CentOS
-OSVERSION 	= 6.4
+OSVERSION 	= 6.5
 UPDATESNAME	= CentOS-Updates
 
 ROLLDIR		= /export/rocks/rolls
 
-CENTOS_VERSION	= \
+CENTOS_VERSION	= 6.5
+UPDATES_VERSION	= 6.5
+CORE_VERSION	= 6.5
+
+# CENTOS_VERSION	= \
 	$(shell rocks list roll $(OSNAME) output-header=no output-col=version | tr -d ' ')
-UPDATES_VERSION	= \
+# UPDATES_VERSION	= \
 	$(shell rocks list roll $(UPDATESNAME) output-header=no output-col=version | tr -d ' ')
-CORE_VERSION	= \
+# CORE_VERSION	= \
 	$(shell rocks list roll cluster-core output-header=no output-col=version | tr -d ' ')
 
 ROLLS		= $(OSNAME),$(CENTOS_VERSION) $(UPDATESNAME),$(UPDATES_VERSION)
